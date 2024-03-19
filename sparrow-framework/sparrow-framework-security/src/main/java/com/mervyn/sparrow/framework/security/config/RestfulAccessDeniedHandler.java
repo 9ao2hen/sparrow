0bd6f4009942d1db8205e46090cf187ceb5e44f6
2hen9ao <1098105375@qq.com>
2024-03-19 21:37:04 +0800

    认证配置

---
+++ sparrow-framework/sparrow-framework-security/src/main/java/com/mervyn/sparrow/framework/security/config/RestfulAccessDeniedHandler.java
@@ -0,0 +1,22 @@
+package com.mervyn.sparrow.framework.security.config;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class RestfulAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException e) throws IOException, ServletException {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+        response.getWriter().flush();
+    }
+}
--- sparrow-module-system/sparrow-module-system-biz/src/main/java/com/mervyn/sparrow/system/security/SecurityConfig.java
+++ sparrow-module-system/sparrow-module-system-biz/src/main/java/com/mervyn/sparrow/system/security/SecurityConfig.java
@@ -1,19 +1,16 @@
 package com.mervyn.sparrow.system.security;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
-@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
+//@Configuration
+//@EnableWebSecurity
+//@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
 
 
